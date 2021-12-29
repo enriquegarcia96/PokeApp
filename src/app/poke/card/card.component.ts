8895bc1b1361cdbbc5a5e511570ba23531d1e518
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-12-29 14:11:18 -0600

    feat: 🎸 input de entreda para la informacion de los pokemones

--- src/app/poke/card/card.component.ts
+++ src/app/poke/card/card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input()
+  info: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
