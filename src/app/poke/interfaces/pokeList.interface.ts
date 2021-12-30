975a019f3c9eecfcd3914dfd1c81e29219c7960f
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-12-30 11:07:34 -0600

    feat: 🎸 interfaces para los endpoint

---
+++ src/app/poke/interfaces/pokeList.interface.ts
@@ -0,0 +1,12 @@
+export interface PokeList{
+  count: number;
+  next:  string;
+  previous: string | null;
+  results: Result[];
+}
+
+
+export interface Result{
+  name: string;
+  url: string;
+}
