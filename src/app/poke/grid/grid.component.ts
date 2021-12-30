cae43a59d373be6578e3a4133d5ea9c1e8f4bfd6
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-12-29 18:18:16 -0600

    refactor: 💡 funciones para el paginado y llamando a pokeService

--- src/app/poke/grid/grid.component.ts
+++ src/app/poke/grid/grid.component.ts
@@ -7,11 +7,27 @@ import { PokeService } from 'src/app/services/poke.service';
   styleUrls: ['./grid.component.css'],
 })
 export class GridComponent {
-  pokeList: any[] = []
+  pokeList: Array<any> = [];
+
+  page: number = 0;
 
   constructor(private pokeService: PokeService) {
-    pokeService.getList().subscribe((res: any) => {
+    this.setData();
+  }
+
+  setData() {
+    this.pokeService.getList(this.page).subscribe((res: any) => {
       this.pokeList = res.results;
     });
+  }
+
+  nextPage() {
+    this.page += 8;
+    this.setData();
+  }
+
+  prevPage() {
+    this.page -= 8;
+    this.setData();
   }
 }
